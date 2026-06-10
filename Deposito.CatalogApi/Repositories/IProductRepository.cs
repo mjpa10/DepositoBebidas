fb628b651f766c2365fcb2e6b0439eaac8ff6c5b
mtheu <m.theus.jose.pereira@gmail.com>
2026-06-10 20:15:01 -0300

    Criacao da interface e classe ProductRepository

---
+++ Deposito.CatalogApi/Repositories/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Deposito.CatalogApi.Models;
+
+namespace Deposito.CatalogApi.Repositories;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetAll();
+    Task<IEnumerable<Product>> GetByCategory(int categoryId);
+    Task<Product> GetById(int id);
+    Task<Product> GetByName(string name);
+    Task<Product> Create(Product product);
+    Task<Product> Update(Product product);
+    Task<Product> Delete(int id);
+}
+
---
+++ Deposito.CatalogApi/Repositories/ProductRepository.cs
@@ -0,0 +1,61 @@
+﻿using Deposito.CatalogApi.Context;
+using Deposito.CatalogApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Deposito.CatalogApi.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly AppDbContext _context;   
+    public ProductRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Product>> GetAll()
+    {
+        return await _context.Products.AsNoTracking().Include(p => p.Category).ToListAsync();
+    }
+
+    public async Task<Product> GetById(int id)
+    {
+        return await _context.Products.Include(p => p.Category).AsNoTracking().FirstOrDefaultAsync(p => p.ProductId == id);
+    }
+
+    public async Task<IEnumerable<Product>> GetByCategory(int id)
+    {
+        return await _context.Products.Include(c => c.Category).Where(p => p.CategoryId == id).AsNoTracking().ToListAsync();
+    }
+    
+    public async Task<Product> GetByName(string name)
+    {
+        return await _context.Products.Include(p => p.Category).AsNoTracking().FirstOrDefaultAsync(p => p.Name == name);
+    }
+
+    public async Task<Product> Create(Product product)
+    {
+        _context.Products.Add(product);
+        await _context.SaveChangesAsync();
+        return product;
+    }
+
+    public async Task<Product> Update(Product product)
+    {
+        _context.Products.Update(product);
+        await _context.SaveChangesAsync();
+        return product;
+    }
+
+    public async Task<Product?> Delete(int id)
+    {
+        var product = await _context.Products.FirstOrDefaultAsync(p => p.ProductId == id);
+
+
+        if (product == null)
+            return null;
+
+        _context.Products.Remove(product);
+        await _context.SaveChangesAsync();
+        return product;
+    }
+}
